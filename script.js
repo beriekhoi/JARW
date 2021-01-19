0a1b1c449d43287eb51c31b58975e45e250932d6
Beriekhoi <65053744+beriekhoi@users.noreply.github.com>
2021-01-19 12:58:41 -0600

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Working with Date and DOM for the Date Section
-let dateElement = document.getElementById("date")
-let dateTextNode = document.createTextNode(new Date())
-dateElement.appendChild(dateTextNode)
