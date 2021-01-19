be4942c3b3116b84ee201b6fd9a449de4f75d1d7
Beriekhoi <65053744+beriekhoi@users.noreply.github.com>
2021-01-19 11:46:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Working with Date and DOM for the Date Section
+let dateElement = document.getElementById("date")
+let dateTextNode = document.createTextNode(new Date())
+dateElement.appendChild(dateTextNode)
